d2b77adece60391ea9cb14fdc995de7dd7fba0b5
spankyf <deanflanagan@gmail.com>
2020-08-27 16:24:50 -0400

    Added pyshell to exController

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -8,6 +8,7 @@ exports.getExercises = async (req, res) => {
   let locals = {};
   locals.hello = "hello";
   locals.now = moment().format(moment.HTML5_FMT.DATETIME_LOCAL);
+
   let options = {
     mode: "text",
     pythonPath: "C:/Users/dean/anaconda3/python.exe",
@@ -35,40 +36,7 @@ exports.getExercises = async (req, res) => {
 };
 
 exports.addExercise = async (req, res) => {
-  let options = {
-    mode: "text",
-    pythonPath: "C:/Users/dean/anaconda3/python.exe",
-  };
-
-  PythonShell.run(
-    "C:/Users/dean/Desktop/coding/music-practice/practice_2.0.py",
-    options,
-    function (err, results) {
-      if (err) {
-        res.status(500).send({
-          error: err,
-        });
-        console.log(err);
-        return;
-      }
-      let obj = JSON.parse(results);
-
-      let locals = {};
-      locals.hello = "hello";
-      locals.now = moment().format(moment.HTML5_FMT.DATETIME_LOCAL);
-
-      res.status(200).render("pages/exercises", {
-        message: "Success",
-        data: obj,
-        localObj: locals,
-      });
-    }
-  );
   const exercise = await db.Exercise.create(req.body);
   console.log("Exercise added to db");
-  res.status(200).render("pages/exercises", {
-    message: "Success",
-    data: obj,
-    localObj: locals,
-  });
+  res.status(201).redirect("back");
 };
